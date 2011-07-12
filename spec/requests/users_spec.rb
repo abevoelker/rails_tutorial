c978b9b0127dd1c4c9abf9707c9a2523fa3e4768
Abe Voelker <abe@abevoelker.com>
2011-07-12 17:23:23 -0500

    Add integration helper and update specs to use it

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -22,15 +22,15 @@ describe "Users" do
     describe "failure" do
 
       it "should not make a new user" do
-	lambda do
+	      lambda do
           visit signup_path
           fill_in "Name",         :with => ""
           fill_in "Email",        :with => ""
           fill_in "Password",     :with => ""
-	  fill_in "Confirmation", :with => ""
-	  click_button
-	  response.should render_template('users/new')
-	  response.should have_selector("div#error_explanation")
+    	    fill_in "Confirmation", :with => ""
+      	  click_button
+      	  response.should render_template('users/new')
+      	  response.should have_selector("div#error_explanation")
         end.should_not change(User, :count)
       end
     end
@@ -40,10 +40,10 @@ describe "Users" do
 
     describe "failure" do
       it "should not sign a user in" do
-        visit signin_path
-        fill_in :email,    :with => ""
-        fill_in :password, :with => ""
-        click_button
+        user = Factory(:user)
+        user.email = ""
+        user.password = ""
+        integration_sign_in user
         response.should have_selector("div.flash.error", :content => "Invalid")
       end
     end
@@ -51,10 +51,7 @@ describe "Users" do
     describe "success" do
       it "should sign a user in and out" do
         user = Factory(:user)
-        visit signin_path
-        fill_in :email,    :with => user.email
-        fill_in :password, :with => user.password
-        click_button
+        integration_sign_in user
         controller.should be_signed_in
         click_link "Sign out"
         controller.should_not be_signed_in
