b93518f9be4ae27e51a8c99a98b1acb6dcf0aba9
Abe Voelker <abe@abevoelker.com>
2011-07-13 01:59:29 -0500

    Prevent admin users from being able to delete themselves (exercise 10.5)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,8 +50,13 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
+    user = User.find(params[:id])
+    if current_user == user then
+      flash[:error] = "You can't delete yourself!"
+    else
+      user.destroy
+      flash[:success] = "User destroyed."
+    end
     redirect_to users_path
   end
 
